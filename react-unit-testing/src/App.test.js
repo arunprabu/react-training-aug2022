c3ce3598923a01ebfde07f63898f6aae579e3dbd
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-08-30 19:00:47 +0530

    updated

--- react-unit-testing/src/App.test.js
+++ react-unit-testing/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Write test spec here
+// Test Pattern: AAA (Arrange, Act, Assert)
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Arrange
+import { render, screen } from '@testing-library/react';
+import App from './App'; // Take up the comp for testing 
+
+// group of related test specs -- Test Suite
+describe('App Component', () => { 
+
+  // test spec / test case 
+  test('should have welcome text', () => {
+    // Act 
+    render(<App />); 
+    const welcomeTextEl = screen.getByText('Welcome to Unit Testing');
+
+    // Assert 
+    expect(welcomeTextEl).toBeInTheDocument();
+  });
+
+  it('should have app comp', () => { // 'it' is an alias of 'test'
+    expect(App).toBeTruthy();
+  });
+
 });
